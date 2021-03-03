c494b57e3056da5f594965d9008cc5f987eab358
hendraaagil <hendraaagil@gmail.com>
2021-03-03 19:06:44 +0700

    Add Up Button to AppBar

--- app/src/main/java/com/example/learnandroidnavigation/OtherActivity.java
+++ app/src/main/java/com/example/learnandroidnavigation/OtherActivity.java
@@ -2,30 +2,16 @@ package com.example.learnandroidnavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class OtherActivity extends AppCompatActivity {
-    private Button btnOpenMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
 
-        btnOpenMain = findViewById(R.id.btnOpenMain);
-        btnOpenMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMainActivity();
-            }
-        });
-    }
-
-    private void openMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        getSupportActionBar().setTitle("Other Activity");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
